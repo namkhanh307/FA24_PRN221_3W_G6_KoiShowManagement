a1e4d33e0977bb52fa0c3af4b7941d1e77037d5a
Nguyen Viet Nam Khanh <nguyenvietnamkhanhbl@gmail.com>
2024-12-08 22:55:48 +0700

    Update

--- KoiShowManagement.Repositories/CompetitionRepository.cs
+++ KoiShowManagement.Repositories/CompetitionRepository.cs
@@ -1,4 +1,5 @@
 using KoiShowManagement.Repositories.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,11 @@ namespace KoiShowManagement.Repositories
 {
     public class CompetitionRepository : GenericRepository<Competition>
     {
+        public async Task<List<Competition>> GetAllAsync()
+        {
+            var referenceKey = await _context.Competitions.Include
+                (m => m.CompetitionType).ToListAsync();
+            return referenceKey;
+        }
     }
 }
